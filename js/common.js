5fcc19592e4890d00915eaa72b4f50b93f94a4f0
Kei037 <juneway@hanmail.net>
2022-04-29 17:50:20 +0900

    2022-04-29 study

---
+++ js/common.js
@@ -0,0 +1,16 @@
+$(function(){
+  $(window).scroll(function(){
+    let windowHeight = $(this).height();
+    let slideHeight = $(".inner").height();
+    let scTop = $(this).scrollTop();
+    console.log($(".inner").offset().top);
+    console.log(scTop);
+    console.log($(".inner").offset().top + windowHeight);
+    $(".inner").each(function(i){
+      let thisOffset = $(this).offset();
+      if ( thisOffset.top <= scTop) {
+        $(".mainTxt").addClass("opc");
+      }
+    })
+  });
+});
