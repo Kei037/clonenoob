2de290e8828dd981f412313c495bfd58c28f4f62
Kei037 <102635544+Kei037@users.noreply.github.com>
2022-05-01 19:51:27 +0900

    2022-05-01 study

--- js/common.js
+++ js/common.js
@@ -1,16 +1,23 @@
 $(function(){
   $(window).scroll(function(){
     let windowHeight = $(this).height();
-    let slideHeight = $(".inner").height();
     let scTop = $(this).scrollTop();
-    console.log($(".inner").offset().top);
-    console.log(scTop);
-    console.log($(".inner").offset().top + windowHeight);
+    let slideHeight = $(".container").height();
+    // console.log($(".inner").offset().top);
+    // console.log(scTop);
+    // console.log($(".inner").offset().top + windowHeight);
     $(".inner").each(function(i){
       let thisOffset = $(this).offset();
       if ( thisOffset.top <= scTop) {
-        $(".mainTxt").addClass("opc");
+        // $(".mainTxt").addClass("on");
+        $(".mainTxt").css({opacity: "1", transform: "translateY(0)"});
+      }else if ( thisOffset.top <= scTop + 13 ) {
+        $(".inner").children('p').css({opacity: "1", transform: "translateY(0)"});
+      }else if ( thisOffset.top <= scTop + 20 ) {
+        $(".list-items ").css({opacity: "1", transform: "translateY(0)"});
+      }else if ( thisOffset.top <= scTop + 23 ) {
+        $("second-div").css({opacity: "1", transform: "translateY(0)"});
       }
-    })
+    });
   });
 });
