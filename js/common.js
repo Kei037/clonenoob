b596d85aa5b62a073ad1a406b531964bb8f95b7f
kei037 <juneway@hanmail.net>
2022-05-03 09:10:08 +0900

    first
    first

--- js/common.js
+++ js/common.js
@@ -9,14 +9,16 @@ $(function(){
     $(".inner").each(function(i){
       let thisOffset = $(this).offset();
       if ( thisOffset.top <= scTop) {
-        // $(".mainTxt").addClass("on");
-        $(".mainTxt").css({opacity: "1", transform: "translateY(0)"});
+        $(".mainTxt").addClass("on");
+        // $(".mainTxt").css({opacity: "1", transform: "translateY(0)"});
       }else if ( thisOffset.top <= scTop + 13 ) {
-        $(".inner").children('p').css({opacity: "1", transform: "translateY(0)"});
+        $(".inner").addClass("on");
       }else if ( thisOffset.top <= scTop + 20 ) {
-        $(".list-items ").css({opacity: "1", transform: "translateY(0)"});
-      }else if ( thisOffset.top <= scTop + 23 ) {
-        $("second-div").css({opacity: "1", transform: "translateY(0)"});
+        $(".list-items ").addClass("on").css({opacity: "1", transform: "translateY(0)"});
+      }else if ( thisOffset.top <= scTop + 21 ) {
+        $("second-div").addClass("on");
+      }else if ( thisOffset.top <= scTop + 25 ) {
+        $("third-div").addClass("on");
       }
     });
   });
