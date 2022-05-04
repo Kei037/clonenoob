536f46b35e4db345b967b4dd34d1b072e6133112
kei037 <juneway@hanmail.net>
2022-05-04 17:45:36 +0900

    2022-05-04 clone

--- js/common.js
+++ js/common.js
@@ -9,17 +9,44 @@ $(function(){
     $(".inner").each(function(i){
       let thisOffset = $(this).offset();
       if ( thisOffset.top <= scTop) {
-        $(".mainTxt").addClass("on");
+        $(".mainTxt").addClass("on").css({opacity: "1", transform: "translateY(0)"});
         // $(".mainTxt").css({opacity: "1", transform: "translateY(0)"});
       }else if ( thisOffset.top <= scTop + 13 ) {
-        $(".inner").addClass("on");
+        $(".inner").addClass("on1").css({opacity: "1", transform: "translateY(0)"});
       }else if ( thisOffset.top <= scTop + 20 ) {
         $(".list-items ").addClass("on").css({opacity: "1", transform: "translateY(0)"});
       }else if ( thisOffset.top <= scTop + 21 ) {
-        $("second-div").addClass("on");
+        $("second-div").addClass("on").css({opacity: "1", transform: "translateY(0)"});
       }else if ( thisOffset.top <= scTop + 25 ) {
-        $("third-div").addClass("on");
+        $("third-div").addClass("on").css({opacity: "1", transform: "translateY(0)"});
       }
     });
   });
+
+/*---------------슬라이드 영역---------------*/
+  let marginLeftNum = $(".slide-list").css("margin-left");
+  console.log(parseInt(marginLeftNum));
+  const marginNumber = 450;
+  let pwElem = $(".slide-list");
+  let pageLeng = $(".slide-list .news").length;
+  $(pwElem).css("width", marginNumber*(pageLeng))
+
+  function slideFunc(el){
+    el.click(function(){
+      marginLeftNum= parseInt(pwElem.css("margin-left"));
+      let isAni = pwElem.is(":animated");
+      if ( !isAni ) {
+        if ( $(el).hasClass("next") && marginLeftNum > -(marginNumber*(pageLeng-1)) ) {
+          pwElem.animate({marginLeft: marginLeftNum - marginNumber}, "fast", function(){});
+        }else if ( $(el).hasClass("prev") && marginLeftNum < 0 ) {
+          pwElem.animate({marginLeft: marginLeftNum + marginNumber}, "fast", function(){});
+        }else if ( marginLeftNum == -(marginNumber*(pageLeng-1)) || marginLeftNum == 0 ) {
+          alert("더 이상 이미지가 없습니다.");
+        }
+      }
+    });
+  }
+  $(".btn").each(function(){
+    slideFunc($(this));
+  })
 });
